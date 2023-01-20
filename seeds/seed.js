356ac83e38129f6783f01a4c5f4163a80b1a646c
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-01-20 13:51:03 -0800

    created seeds file structure for generating dummy data

---
+++ seeds/seed.js
@@ -0,0 +1,14 @@
+const sequelize = require("../config/connection");
+const { User, Prompt, Suggestion, Restriction, Ingredient } = require("../models");
+
+const userData = require("./userData.json");
+const promptData = require("./promptData.json");
+const suggestionData = require("./suggestionData.json");
+const restrictionData = require("./restrictionData.json");
+const ingredientData = require("./ingredientData.json");
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    
+}
