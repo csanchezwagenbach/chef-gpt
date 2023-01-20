8dd81c66e248e77b6e2355454ee2c5934763ac74
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-01-20 14:37:13 -0800

    successfully rendering homepage to main template when making calls to base url. deleted unneccesary code from multiple files to remove bugs in server functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,2 +1,13 @@
 const router = require("express").Router();
 const { User, Prompt, Suggestion, Restriction, Ingredient } = require("../models");
+
+
+router.get("/", async (req, res) => {
+    try{
+        res.render("homepage")
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+module.exports = router;
