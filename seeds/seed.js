cf9342d1fa4b57312f05e4ad63199e1946c301fc
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-01-20 18:23:14 -0800

    successfully seeding users into database

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,10 +2,10 @@ const sequelize = require("../config/connection");
 const { User, Prompt, Suggestion, Restriction, Ingredient } = require("../models");
 
 const userData = require("./userData.json");
-const promptData = require("./promptData.json");
-const suggestionData = require("./suggestionData.json");
-const restrictionData = require("./restrictionData.json");
-const ingredientData = require("./ingredientData.json");
+// const promptData = require("./promptData.json");
+// const suggestionData = require("./suggestionData.json");
+// const restrictionData = require("./restrictionData.json");
+// const ingredientData = require("./ingredientData.json");
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
