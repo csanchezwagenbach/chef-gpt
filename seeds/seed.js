69b0552e7e376bf38bdd9c297f708dde17be70fd
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-01-20 18:30:18 -0800

    created ingredient bulk seed functionality and dummy ingredient data, ready to test seeding process

--- seeds/seed.js
+++ seeds/seed.js
@@ -5,12 +5,17 @@ const userData = require("./userData.json");
 // const promptData = require("./promptData.json");
 // const suggestionData = require("./suggestionData.json");
 // const restrictionData = require("./restrictionData.json");
-// const ingredientData = require("./ingredientData.json");
+const ingredientData = require("./ingredientData.json");
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
     const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true
+    });
+
+    const ingredients = await Ingredient.bulkCreate(ingredientData, {
         individualHooks: true,
         returning: true
     });
