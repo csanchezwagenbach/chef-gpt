aa4eefd0d8da609cc8f92186168d1ca0316622ec
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-01-20 17:30:32 -0800

    added in /login route in controllers/homeroutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,8 +3,16 @@ const { User, Prompt, Suggestion, Restriction, Ingredient } = require("../models
 
 
 router.get("/", async (req, res) => {
-    try{
+    try {
         res.render("homepage")
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+router.get("/login", async (req, res) => {
+    try {
+        res.render("login")
     } catch (err) {
         res.status(500).json(err)
     }
