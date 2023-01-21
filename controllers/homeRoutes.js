525f1b55613701a5bcd7d7743845f2b1a418a1e0
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-01-20 18:05:12 -0800

    built out majority of homeroutes, awaiting testing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,9 +13,30 @@ router.get("/", async (req, res) => {
 router.get("/login", async (req, res) => {
     try {
         res.render("login")
+        // Potentially put in a logic check that will redirect users to their dashboard page if they are already logged in and they try to navigate here
     } catch (err) {
         res.status(500).json(err)
     }
 });
+
+router.get("/:user_id", async (req, res) => {
+    try {
+        res.render("profile")
+        // This route will be fleshed out with information that is sent to the template once the user database is seeded and api routes are all functional
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+router.get("/:user_id/:suggestion_id", async (req, res) => {
+    try {
+        res.render("suggestions")
+        // This route will be fleshed out with information that is sent to the template once the user database and suggestions database is seeded and api routes are all functional
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+// One more view to be created that will be request.handlebars, this will be a get request to /:user_id/newrequest
 
 module.exports = router;
