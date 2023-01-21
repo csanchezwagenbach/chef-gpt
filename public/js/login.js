320e6832d46a906cadee1042627e7038afdd78cc
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-01-20 21:01:46 -0800

    successfully created a user in the database from the front end and logged into that user from the front end

--- public/js/login.js
+++ public/js/login.js
@@ -2,20 +2,20 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
   
     // Collect values from the login form
-    const username = document.querySelector('#name-login').value.trim();
+    const name = document.querySelector('#name-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
-    if (username && password) {
+    if (name && password) {
       // Send a POST request to the API endpoint
       const response = await fetch('/api/users/login', {
         method: 'POST',
-        body: JSON.stringify({ username, password }),
+        body: JSON.stringify({ name, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('/profile');
+        document.location.replace('/');
       } else {
         alert(response.statusText);
       }
@@ -36,7 +36,8 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/profile');
+        console.log(response)
+        // document.location.replace('/profile');
       } else {
         alert(response.statusText);
       }
