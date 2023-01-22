573f063b68b7ad6d57eb425d7b83717d58b0c4de
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-01-21 18:11:05 -0800

    added in relationships describing prompt's relationship to ingredient/restriction and suggestion's relationship to prompt, ready to test

--- models/index.js
+++ models/index.js
@@ -20,48 +20,65 @@ Ingredient.belongsToMany(User, {
     unique: false,
   },
 });
-Ingredient.belongsToMany(Restriction, {
-  through: {
-    model: Prompt,
-    unique: false,
-  },
-});
-Restriction.belongsToMany(Ingredient, {
-  through: {
-    model: Prompt,
-    unique: false,
-  },
-});
+// Ingredient.belongsToMany(Restriction, {
+//   through: {
+//     model: Prompt,
+//     unique: false,
+//   },
+// });
+// Restriction.belongsToMany(Ingredient, {
+//   through: {
+//     model: Prompt,
+//     unique: false,
+//   },
+// });
 Restriction.belongsToMany(User, {
   through: {
     model: Prompt,
     unique: false,
   },
 });
+
 User.belongsToMany(Restriction, {
   through: {
     model: Prompt,
     unique: false,
   },
 });
+
 Prompt.belongsToMany(User, {
   through: {
     model: Suggestion,
     unique: false,
   },
 });
+
 User.belongsToMany(Prompt, {
   through: {
     model: Suggestion,
     unique: false,
   },
 });
+
 Suggestion.belongsTo(User, {
   foreignKey: "user_id",
 });
+
 User.hasMany(Suggestion, {
   foreignKey: "user_id",
   onDelete: "CASCADE",
+});
+
+Suggestion.belongsTo(Prompt, {
+  foreignKey: "prompt_id"
+});
+
+Prompt.belongsTo(Ingredient, {
+  foreignKey: "ingredient_id"
+});
+
+Prompt.belongsTo(Restriction, {
+  foreignKey: "restriction_id"
 });
 
 module.exports = { Ingredient, Prompt, Restriction, Suggestion, User };
