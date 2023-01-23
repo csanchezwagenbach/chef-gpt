d88c646f539c21e621a589f6d1bd8533cb1b607e
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-01-23 11:52:47 -0800

    sending up event listener that triggers fetch to api, merging with colleague's work who is ready to test api routes

---
+++ public/js/save.js
@@ -0,0 +1,24 @@
+let saveButton = document.querySelector("#save-button");
+
+let titleInput = document.querySelector("#new-suggestion-title");
+let contentInput = document.querySelector("#new-suggestion-content");
+
+let name = titleInput.value;
+let title = contentInput.value;
+
+async function saveSuggestion () {
+    if (!title) {
+        return;
+    }
+    const response = await fetch(`/api/suggestions`, {
+        method: "POST",
+        body: JSON.stringify({title, content}),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    const resolved = response.json()
+    console.log(resolved)
+}
+
+saveButton.addEventListener("click", saveSuggestion());
