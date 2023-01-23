d77f6cabff1e3cc57f0e6e0cfa5c09db7520ddf8
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-01-22 16:32:33 -0800

    successfully adding and deleting restrictions/ingredients to running lists on the page, ready to add in search button

--- public/js/request.js
+++ public/js/request.js
@@ -2,8 +2,12 @@ let ingredientInput = document.querySelector("#ingredient-input");
 let addIngredientButton = document.querySelector("#add-ingredient");
 let ingredientsList = document.querySelector("#ingredient-list");
 
+let restrictionInput = document.querySelector("#restriction-input");
+let addRestrictionButton = document.querySelector("#add-restriction");
+let restrictionsList = document.querySelector("#restriction-list");
 
 let ingredients = [];
+let restrictions = [];
 
 function addIngredient () {
     let ingredient = ingredientInput.value;
@@ -15,9 +19,9 @@ function addIngredient () {
     ingredients.forEach(ingredient => {
         let listItem = document.createElement("li");
         listItem.textContent = ingredient;
-        ingredientsList.append(listItem)
+        ingredientsList.append(listItem);
         listItem.innerHTML +='<span onclick="deleteIngredient(this)" style="float:right;cursor:pointer;">X</span>';
-    })
+    });
 }
 
 function deleteIngredient (ingredient) {
@@ -30,4 +34,31 @@ function deleteIngredient (ingredient) {
     ingredient.parentElement.remove();
 }
 
+function addRestriction () {
+    let restriction = restrictionInput.value;
+    let restrictionId = restrictionInput.getAttribute("data-number");
+    if (!restriction) {
+        return;
+    }
+    restrictions.push(restriction);
+    restrictionsList.innerHTML = "";
+    restrictions.forEach(restriction => {
+        let listItem = document.createElement("li");
+        listItem.textContent = restriction;
+        listItem.setAttribute("data-number", restrictionId)
+        restrictionsList.append(listItem);
+        listItem.innerHTML += '<span onclick="deleteRestriction(this)" style="float:right;cursor:pointer;">X</span>';
+    });
+}
+
+function deleteRestriction (restriction) {
+    for (let i = 0; i < restrictionsList.childElementCount; i++) {
+        if (restrictionsList.children[i] === restriction.parentElement) {
+            restrictions.splice(i, 1);
+        }
+    }
+    restriction.parentElement.remove();
+}
+
 addIngredientButton.addEventListener("click", addIngredient);
+addRestrictionButton.addEventListener("click", addRestriction);
