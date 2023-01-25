e7f161bcc74faac4c0018f502ecde6873bb0d48a
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-01-24 19:41:44 -0800

    added comments and deleted unneccessary code in controllers/index and controllers/homeRoutes

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 
+// This page serves to direct route traffic. Users are either directed to homeroutes, holding all front-end routes, or apiRoutes, where the user is directed to the index folder within the api folder. 
+
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
 
